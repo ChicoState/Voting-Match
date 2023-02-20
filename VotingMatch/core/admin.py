6e570818cb8895006aef7678b206a91f17aa4909
no-bugs-only-features <dwright2@mail.csuchico.edu>
2023-02-20 15:43:48 -0800

    Add basic candidate model

--- VotingMatch/core/admin.py
+++ VotingMatch/core/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
 # admin.site.register(Model)
+admin.site.register(models.Candidate)
--- VotingMatch/core/models.py
+++ VotingMatch/core/models.py
@@ -1,3 +1,36 @@
+# Documentation about Django models:
+# https://docs.djangoproject.com/en/4.1/topics/db/models/
+#
+# Model fields:
+# https://docs.djangoproject.com/en/4.1/ref/models/fields/
+
 from django.db import models
 
-# Create your models here.
+class Candidate(models.Model):
+    STATES = [
+        ('AL', 'Alabama'), ('AK', 'Alaska'), ('AZ', 'Arizona'), ('AR', 'Arkansas'), ('CA', 'California'),
+        ('CO', 'Colorado'), ('CT', 'Connecticut'), ('DE', 'Delaware'), ('DC', 'District of Columbia'),
+        ('FL', 'Florida'), ('GA', 'Georgia'), ('HI', 'Hawaii'), ('ID', 'Idaho'), ('IL', 'Illinois'),
+        ('IN', 'Indiana'), ('IA', 'Iowa'), ('KS', 'Kansas'), ('KY', 'Kentucky'), ('LA', 'Louisiana'),
+        ('ME', 'Maine'), ('MD', 'Maryland'), ('MA', 'Massachusetts'), ('MI', 'Michigan'), ('MN', 'Minnesota'),
+        ('MS', 'Mississippi'), ('MO', 'Missouri'), ('MT', 'Montana'), ('NE', 'Nebraska'), ('NV', 'Nevada'),
+        ('NH', 'New Hampshire'), ('NJ', 'New Jersey'), ('NM', 'New Mexico'), ('NY', 'New York'),
+        ('NC', 'North Carolina'), ('ND', 'North Dakota'), ('OH', 'Ohio'), ('OK', 'Oklahoma'), ('OR', 'Oregon'),
+        ('PA', 'Pennsylvania'), ('RI', 'Rhode Island'), ('SC', 'South Carolina'), ('SD', 'South Dakota'),
+        ('TN', 'Tennessee'), ('TX', 'Texas'), ('UT', 'Utah'), ('VT', 'Vermont'), ('VA', 'Virginia'),
+        ('WA', 'Washington'), ('WV', 'West Virginia'), ('WI', 'Wisconsin'), ('WY', 'Wyoming')
+    ]
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    state = models.CharField(max_length=2, choices=STATES)
+
+
+# class Issue(models.Model):
+
+# Cross table model reference:
+# https://stackoverflow.com/questions/69687277/how-to-add-custom-field-in-manytomany-through-table-in-django
+# class CandidateOpinion(models.Model):
+
+# class UserOpinion(models.Model):
+
+# class CandidateScore(models.Model):
