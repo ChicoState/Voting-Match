4c73bbb225e25ee088753fe9e7397bce39eb6f83
no-bugs-only-features <dwright2@mail.csuchico.edu>
2023-02-20 15:15:04 -0800

    Update formatting issues

--- VotingMatch/core/urls.py
+++ VotingMatch/core/urls.py
@@ -1,5 +1,5 @@
-#Documentation about Django urls:
-#https://docs.djangoproject.com/en/4.1/topics/http/urls/
+# Documentation about Django urls:
+# https://docs.djangoproject.com/en/4.1/topics/http/urls/
 
 from django.urls import path
 from . import views
